af44084e00a39971ff57347798e62ab2de90d9cb
ShubhamPal19 <shubhampal2309@gmail.com>
2022-01-11 13:23:24 +0530

    Frontend started

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>this is app</h1>
+     
+    
+
+      <Router>
+        <Navbar/>
+
+        <Routes>
+          <Route  path="/about" element={<About />} />
+
+          <Route index path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
